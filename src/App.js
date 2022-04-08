7940bf6ffb8018d390b4f93f905e9280853003d6
Robel-Ak <Robelamde21@gmail.com>
2022-04-08 23:13:16 +0300

    Class Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import { Component } from 'react';
+import Welcome from './components/Welcome';
 
 class App extends Component { //in the latest react app we start our app with function but we have changed it to class component.
   render () {
     return (
       <div className="App">
         <Greet/>
+        <Welcome/>
       </div>
     );
   }
