7a7f60eab1dcfec6243283ef5d7aa07df4ccd06e
Robel-Ak <Robelamde21@gmail.com>
2022-04-09 14:54:39 +0300

    setState

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Component } from 'react';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 class App extends Component { //in the latest react app we start our app with function but we have changed it to class component.
   render () {
     return (
       <div className="App">
-        <Message />
+        <Counter />
+        {/* <Message /> */}
         {/* <Greet name="Bruce" heroName ="Batman"/>
           <p>This is a children props of Bruce</p>
         <Greet name="Clark" heroName ="Superman"/>
