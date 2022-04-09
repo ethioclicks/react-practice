722d7a079cdb0bc0bf5e8b831c720d27f4220802
Robel-Ak <Robelamde21@gmail.com>
2022-04-09 14:30:11 +0300

    state

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import Greet from './components/Greet';
 import { Component } from 'react';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message';
 
 class App extends Component { //in the latest react app we start our app with function but we have changed it to class component.
   render () {
     return (
       <div className="App">
-        <Greet name="Bruce" heroName ="Batman"/>
+        <Message />
+        {/* <Greet name="Bruce" heroName ="Batman"/>
           <p>This is a children props of Bruce</p>
         <Greet name="Clark" heroName ="Superman"/>
           <button>Action</button>
         <Greet name="Diana" heroName ="Wonder Women"/>
         <Welcome name="Bruce" heroName ="Batman"/>
         <Welcome name="Clark" heroName ="Superman"/>
-        <Welcome name="Diana" heroName ="Wonder Women"/>
+        <Welcome name="Diana" heroName ="Wonder Women"/> */}
+        
         {/* <Hello/> */}
       </div>
     );
