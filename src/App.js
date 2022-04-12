ce01cfe19c1fd090071c0f5e9bd2f671b727d3ad
Robel-Ak <Robelamde21@gmail.com>
2022-04-12 19:47:34 +0300

    Event Handling

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
-
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 class App extends Component { //in the latest react app we start our app with function but we have changed it to class component.
   render () {
     return (
       <div className="App">
+        <FunctionClick />
+        <ClassClick />
         {/* <Counter /> */}
         {/* <Message /> */}
         {/* <Greet name="Bruce" heroName ="Batman"/>
           <p>This is a children props of Bruce</p>
         <Greet name="Clark" heroName ="Superman"/>
           <button>Action</button>*/}
-        <Greet name="Diana" heroName ="Wonder Women"/>
-        <Welcome name="Bruce" heroName ="Batman"/>
+        {/* <Greet name="Diana" heroName ="Wonder Women"/>
+        <Welcome name="Bruce" heroName ="Batman"/> */}
         {/* <Welcome name="Clark" heroName ="Superman"/>
         <Welcome name="Diana" heroName ="Wonder Women"/>  */}
         
