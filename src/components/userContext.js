6d7a77432573321e016f2a9f74f2b45e8fd84088
Robel-Ak <Robelamde21@gmail.com>
2022-05-04 18:24:06 +0300

    useState Hook

--- src/components/userContext.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-
-const UserContext = React.createContext() 
-const UserProvider = UserContext.Provider
-const UserConsumer = UserContext.Consumer
-
-export {UserProvider, UserConsumer};
-export default UserContext
