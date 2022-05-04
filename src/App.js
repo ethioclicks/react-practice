b58c3dbfc7eb73405726c07921e6e1dabc74bfd7
Robel-Ak <Robelamde21@gmail.com>
2022-05-04 18:58:21 +0300

    useState with array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { Component } from 'react';
-import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/HookCounterFour';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <HookCounterThree/>
+        <HookCounterFour/>
       </div>
     );
   }
