3624de29b26feaae1c4ff853e382f3bf92f49c5a
Robel-Ak <Robelamde21@gmail.com>
2022-05-04 20:21:11 +0300

    Fetching data with useEffect part 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { Component } from 'react';
-import IntervalHookCounter from './components/IntervalHookCounter';
+import DataFetching from './components/DataFetching';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <IntervalHookCounter/>
+        <DataFetching/>
       </div>
     );
   }
