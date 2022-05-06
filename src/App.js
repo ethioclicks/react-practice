f02e63d8c0ad27f713f212140c2ffa419d873806
Robel-Ak <Robelamde21@gmail.com>
2022-05-06 19:55:11 +0300

    useReducer Complex state & action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React, { Component } from 'react';
 import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <CounterOne/>
+      {/* <CounterOne/> */}
+      <CounterTwo/>
     </div>
   );
 }
