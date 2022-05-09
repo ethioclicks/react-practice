96a6182ce731ab7cb9b7e2561a6675896ca442dd
Robel-Ak <Robelamde21@gmail.com>
2022-05-09 19:00:50 +0300

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import React from 'react';
-import DataFetchingOne from './components/DataFetchingOne';
-import DataFetchingTwo from './components/DataFetchingTwo';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-       {/* <DataFetchingOne /> */}
-       <DataFetchingTwo />
+       <ParentComponent />
     </div>
    
   );
