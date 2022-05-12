78530ecec06eb178a5322234b317aedb775b276b
Robel-Ak <Robelamde21@gmail.com>
2022-05-12 19:19:12 +0300

    Render useMemo and useCallback

--- src/components/Incorrect Optimizations/ParentFour.js
+++ src/components/Incorrect Optimizations/ParentFour.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 import { MemoizedChildFive } from './ChildFive';
 
 const ParentFour = () => {
@@ -8,13 +8,18 @@ const ParentFour = () => {
         fname: 'Bruce',
         lname: 'Wayne'
     }
+    const memoizedPerson = useMemo(() => person, [])
+
     const handleClick = () => {}
+
+    const memoizedhandleClick = useCallback(handleClick, [])
     console.log('ParentFour Render')
     return (
         <div>
             <button onClick={() => setCount((c) => c + 1)}>Count - {count}</button>
             <button onClick={() => setName('World')}>Change name</button>
-            <MemoizedChildFive name={name} handleClick={handleClick}/>
+            {/* <MemoizedChildFive name={name} person={memoizedPerson}/> */}
+            <MemoizedChildFive name={name} handleClick={memoizedhandleClick}/>
         </div>
     )
 }
