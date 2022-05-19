fed95eb70e8cc418a907efd66ce2751c08c0a379
Robel-Ak <Robelamde21@gmail.com>
2022-05-19 18:43:32 +0300

    TypeScript Advanced props

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Greet } from './Components/Greet';
-import { Person } from './Components/Person';
-import { PersonList } from './Components/PersonList';
+import { Heading } from './Components/Heading';
+import { Oscar } from './Components/Oscar';
+import { Status } from './Components/Status';
 
 function App() {
-  const PersonName = {
-    first: 'Bruce',
-    last: 'Wayne'
-  }
-
-  const nameList = [
-    {
-      first: 'Bruce',
-      last: 'Wayne',
-    },
-    {
-      first: 'Clark',
-      last: 'Kent',
-    },
-    {
-      first: 'Princess',
-      last: 'Diana',
-    },
-  ]
   return (
     <div className="App">
-      <Greet name='Hello' messageCount= {20} isLoggedIn= {true} />
-      <Person name= {PersonName} />
-      <PersonList names={nameList} />
+      <Status status='loading'/>
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>
+          Oscar goes to Leonardo Dicaprio!
+        </Heading>
+      </Oscar>
+      <Greet name='Hello'  isLoggedIn={false} />
     </div>
   );
 }
--- src/Components/Greet.tsx
+++ src/Components/Greet.tsx
@@ -1,16 +1,17 @@
 type Greetprops = {
     name: string
-    messageCount:number
+    messageCount?: number
     isLoggedIn: boolean
 } 
 export const Greet = (props: Greetprops) => {
+    const { messageCount = 0 } = props
   return (
     <div>
         <h2>Welcome {props.name}! </h2>
-        <h2>The message count is  {props.messageCount}! </h2>
+        <h2>The message count is  {messageCount}! </h2>
         <h2>
             {
-                props.isLoggedIn ? `Welcome ${props.name}! you have ${props.messageCount} unread messages ` : 'Welcome Guest'
+                props.isLoggedIn ? `Welcome ${props.name}! you have ${messageCount} unread messages ` : 'Welcome Guest'
             }
         </h2>
     </div>
