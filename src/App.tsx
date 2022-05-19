412a433252646cf1542fcbc1adba680e6a0dbb64
Robel-Ak <Robelamde21@gmail.com>
2022-05-19 19:07:25 +0300

    TypeScript Style Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import './App.css';
-import { Button } from './Components/Button';
-import { Input } from './Components/Input';
-
+import { Container } from './Components/Container';
 function App() {
   return (
     <div className="App">
-      <Button handleclick={(event, id) => {
-        console.log('Button clicked', event, id)
-      }}/>
-      <Input value='' handleChange={(event) => console.log(event)}/>
+      <Container styles={{ border: '1px solid black', padding: '1rem' }}/>
     </div>
   );
 }
