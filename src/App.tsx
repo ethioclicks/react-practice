3cbe08a3286c2ffd6b95b1a3c1974462e8e1a8c6
Robel-Ak <Robelamde21@gmail.com>
2022-05-20 17:14:30 +0300

    TypeScript Restriciting Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
 import './App.css';
-import { List } from './Components/generics/List';
+import { RandomNumbers } from './Components/restriction/RandomNumbers';
 function App() {
   return (
     <div className="App">
-      <List 
-          items={['Batman', 'Superman', 'Wonder Woman']}
-          onClick={(item) => console.log(item)}
-      />
-      {/* <List 
-          items={[1, 2, 3]}
-          onClick={(item) => console.log(item)}
-      />
-      <List 
-          items={[
-            {
-              id: 1,
-              first: 'Bruce',
-              last: 'Wayne',
-            },
-            {
-              id: 2,
-              first: 'Clark',
-              last: 'Kent',
-            },
-            {
-              id: 3,
-              first: 'Princess',
-              last: 'Diana',
-            },
-          ]}
-          onClick={(item) => console.log(item)}
-      /> */}
+      <RandomNumbers  value={10} isNegative={true} />
     </div>
   );
 }
