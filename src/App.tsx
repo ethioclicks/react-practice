6d110fcdda5691e93cc84f6714344bdb6b4542eb
Robel-Ak <Robelamde21@gmail.com>
2022-05-20 15:55:52 +0300

    TypeScript Generic props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import './App.css';
-import { Private } from './Components/Auth/Private';
-import { Profile } from './Components/Auth/Profile';
+import { List } from './Components/generics/List';
 function App() {
   return (
     <div className="App">
-      <Private isLoggedIn={true} component={Profile} />
+      <List 
+          items={['Batman', 'Superman', 'Wonder Woman']}
+          onClick={(item) => console.log(item)}
+      />
+      {/* <List 
+          items={[1, 2, 3]}
+          onClick={(item) => console.log(item)}
+      />
+      <List 
+          items={[
+            {
+              id: 1,
+              first: 'Bruce',
+              last: 'Wayne',
+            },
+            {
+              id: 2,
+              first: 'Clark',
+              last: 'Kent',
+            },
+            {
+              id: 3,
+              first: 'Princess',
+              last: 'Diana',
+            },
+          ]}
+          onClick={(item) => console.log(item)}
+      /> */}
     </div>
   );
 }
